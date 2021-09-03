3b018ce380d0014d268677d86782851bac26dc08
kasperworks <63425079+kasperworks@users.noreply.github.com>
2021-09-03 23:49:12 +0300

    First paint tweaks

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -21,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
       display: flex;
       flex-direction: column;
       min-height: 100vh;
+      background-color: var(--background);
     }
 
     html {
