a442693ed7952a5b197c2835e734a9dfeb6eb051
kasperworks <63425079+kasperworks@users.noreply.github.com>
2021-09-05 02:07:42 +0300

    Some initial content drafting

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,15 @@ import { getAllPosts } from '../lib/mdx';
 import * as s from '../styles/Layout.styled';
 
 export default function BlogList({ posts }) {
+  const postsByDate = posts
+    .sort(
+      (a, b) => new Date(b.frontmatter.publishedAt) - new Date(a.frontmatter.publishedAt),
+    )
+    .slice(0, 3);
+
   return (
     <Container>
-      <h1>kasper.works</h1>
+      <h1>Kasper Viita</h1>
       <s.IndexTextBlock>
         <s.IntroText>
           <p>Hi! I&apos;m Kasper, a journalist and a programmer.</p>
@@ -15,16 +21,25 @@ export default function BlogList({ posts }) {
 
       <s.IndexTextBlock>
         <p>
-          This is my digital garden. You&apos;ll find some thoughts and scattered notes,
-          mostly about coding.
+          This is my hub, my digital garden. You&apos;ll find some thoughts and scattered
+          notes, largely about my journey to master the skills of a web application
+          engineer.
         </p>
       </s.IndexTextBlock>
-      <h3>
-        Here are a few links, these ones built by <code>mdx-bundler</code>.
-      </h3>
+      <s.IndexTextBlock>
+        <p>
+          This site is built with Next.js, MDX and React. If you&apos;d like to read about
+          that, see{' '}
+          <em>
+            <Link href="/blog/how-i-built-this-site">here</Link>
+          </em>
+          .
+        </p>
+      </s.IndexTextBlock>
+      <h3>Here are three of the most recent posts</h3>
       <s.IndexTextBlock>
         <ul>
-          {posts.map((post, index) => (
+          {postsByDate.map((post, index) => (
             // eslint-disable-next-line react/no-array-index-key
             <li key={index}>
               <Link href={`/blog/${post.slug}`}>{post.frontmatter.title}</Link>
