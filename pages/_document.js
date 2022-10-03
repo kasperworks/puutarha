5ed4cc92ee4b619e293f0b2861b70e89b579230b
Kasper Viita <neliraituri@gmail.com>
2022-10-03 17:05:53 +0300

    Testing plausible

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script
+            defer
+            data-domain="kasper.works"
+            src="https://plausible.io/js/plausible.js"
+          />
           <link
             rel="preload"
             href="/fonts/josefin-sans-v17-latin-regular.woff2"
