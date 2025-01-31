16724ab4270af36768398be8f44a0b4b6e18e10c
Kasper Viita <kasper.viita@yle.fi>
2025-01-31 15:38:16 +0200

    more conversion work on blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,29 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob, file } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    summary: z.string(),
+    // Transform string to Date object
+    publishedAt: z.string(),
+    // Optional fields
+    lastUpdated: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
-export const collections = { blog };
+const works = defineCollection({
+  loader: file("src/content/works/coding_works.json"),
+  schema: z.object({
+    title: z.string(),
+    url: z.string(),
+    what: z.string(),
+    description: z.string(),
+  }),
+});
+
+export const collections = { blog, works };
